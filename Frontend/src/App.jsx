4e8233f47c188a2f2ae294518ce7a47776ae878c
attariqazhar <18221043@mahasiswa.itb.ac.id>
2024-05-31 18:01:32 +0700

    ...

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card ">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="bg-black">
         Click on the Vite and React logos to learn more
       </p>
     </>
